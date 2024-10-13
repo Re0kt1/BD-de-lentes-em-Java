4477afa456b2dfcda38c0052128d86c0bddc7ab3
Re0kt1 <teco_master@yahoo.com.br>
2024-10-13 13:03:05 -0300

    Update Cliente.java

--- UwU/src/main/java/com/mycompany/uwu/Cliente.java
+++ UwU/src/main/java/com/mycompany/uwu/Cliente.java
@@ -4,26 +4,24 @@ package com.mycompany.uwu;
 
 class Cliente extends Lente{
 
-    private String nomeCliente;
+    /*private String nomeCliente;
         
-    /**
      * Get the value of nome
      *
      * @return the value of nome
-     */
+     
     private String getNome() {
         return nomeCliente;
     }
 
-    /**
      * Set the value of nome
      *
      * @param nome new value of nome
-     */
+     
     protected void setNome(String nomeCliente) {
         this.nomeCliente = nomeCliente;
     }
-    
+    */
     protected void diminuirEstoque(Lente x, int y) {
         x.diminuirEstoque(y);
     }
